ea1ddeed036418aa13f0227b4ab188b1d45b2b43
pipi <nopparat.mu@vru.ac.th>
2023-07-03 17:15:14 +0700

    week2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { SlicePipe } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SlideComponent } from './site/slide/slide.component';
+import { DeveloperComponent } from './site/developer/developer.component';
+import { EquipComponent } from './site/equip/equip.component';
+import { StepComponent } from './site/step/step.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component:SlideComponent},
+  {path : 'dev', component:DeveloperComponent},
+  {path : 'step', component:StepComponent},
+  {path : 'equip', component:EquipComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './site/navbar/navbar.component';
+import { SlideComponent } from './site/slide/slide.component';
+import { FooterComponent } from './site/footer/footer.component';
+import { LoginComponent } from './site/login/login.component';
+import { RegisterComponent } from './site/register/register.component';
+import { DeveloperComponent } from './site/developer/developer.component';
+import { StepComponent } from './site/step/step.component';
+import { EquipComponent } from './site/equip/equip.component';
+import { SiteComponent } from './site/site.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SlideComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    DeveloperComponent,
+    StepComponent,
+    EquipComponent,
+    SiteComponent
   ],
   imports: [
     BrowserModule,
